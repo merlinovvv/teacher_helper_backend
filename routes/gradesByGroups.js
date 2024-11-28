6444e51abb5fa2e3594ad90cc5dba53c38dc1cb5
merlinovvv <merlinovandrej@gmail.com>
2024-11-28 23:16:28 +0200

    .

--- routes/gradesByGroups.js
+++ routes/gradesByGroups.js
@@ -8,7 +8,7 @@ const Classes = require('../models/Classes');
 const Groups = require('../models/Groups');
 const GradesGroupsReports = require('../models/GradesGroupsReports');
 const authenticate = require('../middleware/authenticate');
-const {getSuccessResponse, getErrorResponse, calculateAverage} = require("../utils/functions");
+const { getSuccessResponse, getErrorResponse, calculateAverage } = require("../utils/functions");
 const { log } = require('console');
 
 const storage = multer.diskStorage({
@@ -17,30 +17,30 @@ const storage = multer.diskStorage({
         cb(null, 'uploads/'); // Папка 'uploads'
     },
     filename: function (req, file, cb) {
-        const {subject, schoolClass} = req.query;
+        const { subject, schoolClass } = req.query;
         const userId = req.user.id
 
         cb(null, `${userId}_${subject}_${schoolClass}.xlsx`); // 'custom_name_' + уникальный суффикс
     }
 });
 
-const upload = multer({storage: storage});
+const upload = multer({ storage: storage });
 
 const router = express.Router();
 
 router.get('/subjects', authenticate, async (req, res) => {
-    const subjects = await Subjects.find({userId: req?.user.id});
+    const subjects = await Subjects.find({ userId: req?.user.id });
 
     try {
-        res.status(200).json(getSuccessResponse({subjects}));
+        res.status(200).json(getSuccessResponse({ subjects }));
     } catch (err) {
         res.status(500).json(getErrorResponse('Error'));
     }
 });
 
 router.post('/subjects', authenticate, async (req, res) => {
-    const {name} = req.body;
-    const subject = new Subjects({name, userId: req.user.id});
+    const { name } = req.body;
+    const subject = new Subjects({ name, userId: req.user.id });
     await subject.save();
 
     try {
@@ -51,7 +51,7 @@ router.post('/subjects', authenticate, async (req, res) => {
 });
 
 router.delete('/subjects', authenticate, async (req, res) => {
-    const {_id} = req.query;
+    const { _id } = req.query;
 
     try {
         const existingSubject = await Subjects.findById(_id);
@@ -69,32 +69,32 @@ router.delete('/subjects', authenticate, async (req, res) => {
 });
 
 router.get('/classes', authenticate, async (req, res) => {
-    const classes = await Classes.find({userId: req?.user.id});
+    const classes = await Classes.find({ userId: req?.user.id });
 
     try {
-        res.status(200).json(getSuccessResponse({classes}));
+        res.status(200).json(getSuccessResponse({ classes }));
     } catch (err) {
         res.status(500).json(getErrorResponse('Error'));
     }
 });
 
 router.post('/classes', authenticate, async (req, res) => {
-    const {name, _id} = req.body;
+    const { name, _id } = req.body;
 
     try {
         const existingClass = await Classes.findById(_id);
         console.log(existingClass);
-        
+
         if (existingClass) {
             await Classes.findOneAndUpdate(
-                {_id},
-                {name},
-                {upsert: true, new: true}
+                { _id },
+                { name },
+                { upsert: true, new: true }
             );
 
             res.status(200).json(getSuccessResponse({}, `${name} успішно оновлений!`));
         } else {
-            const classes = new Classes({name, userId: req.user.id});
+            const classes = new Classes({ name, userId: req.user.id });
             await classes.save();
 
             res.status(200).json(getSuccessResponse({}, `${name} успішно доданий!`));
@@ -102,13 +102,13 @@ router.post('/classes', authenticate, async (req, res) => {
 
     } catch (err) {
         console.log(err);
-        
+
         res.status(500).json(getErrorResponse('Error'));
     }
 });
 
 router.delete('/classes', authenticate, async (req, res) => {
-    const {_id} = req.query;
+    const { _id } = req.query;
 
     try {
         // Проверяем, существует ли класс
@@ -134,7 +134,7 @@ router.post('/groups', authenticate, async (req, res) => {
         const results = [];
 
         // Извлекаем все `subject` и `schoolClass` из body
-        const newLinks = groupsArray.map(({subject, schoolClass}) => ({
+        const newLinks = groupsArray.map(({ subject, schoolClass }) => ({
             subject,
             schoolClass,
         }));
@@ -153,16 +153,16 @@ router.post('/groups', authenticate, async (req, res) => {
 
         // Удаляем записи, которые отсутствуют в новом массиве
         if (linksToDelete.length > 0) {
-            const deleteCriteria = linksToDelete.map(({subject, schoolClass}) => ({
+            const deleteCriteria = linksToDelete.map(({ subject, schoolClass }) => ({
                 subject,
                 schoolClass,
             }));
-            await Groups.deleteMany({$or: deleteCriteria});
+            await Groups.deleteMany({ $or: deleteCriteria });
         }
 
         // Обрабатываем или создаем новые записи
         for (const groupData of groupsArray) {
-            const {subject, schoolClass, groups} = groupData;
+            const { subject, schoolClass, groups } = groupData;
 
             // Проверяем, существует ли указанный Subject и Class
             const subjectExists = await Subjects.findById(subject);
@@ -178,15 +178,15 @@ router.post('/groups', authenticate, async (req, res) => {
 
             // Обновляем или создаем запись
             const updatedGroup = await Groups.findOneAndUpdate(
-                {subject, schoolClass, userId: req.user.id},
-                {subject, schoolClass, groups},
-                {upsert: true, new: true}
+                { subject, schoolClass, userId: req.user.id },
+                { subject, schoolClass, groups },
+                { upsert: true, new: true }
             );
 
             results.push(updatedGroup);
         }
 
-        res.status(200).json(getSuccessResponse({results}, 'Групи оцінок успішно оновлені'));
+        res.status(200).json(getSuccessResponse({ results }, 'Групи оцінок успішно оновлені'));
     } catch (err) {
         res.status(500).json(getErrorResponse('Помилка оновлення групи оцінок'));
     }
@@ -194,9 +194,9 @@ router.post('/groups', authenticate, async (req, res) => {
 
 router.get('/groups', authenticate, async (req, res) => {
     try {
-        const groups = await Groups.find({userId: req?.user.id});
+        const groups = await Groups.find({ userId: req?.user.id });
 
-        res.status(201).json(getSuccessResponse({groups}));
+        res.status(201).json(getSuccessResponse({ groups }));
     } catch (err) {
         console.error('Error creating groups:', err);
         res.status(500).json(getErrorResponse('Error creating groups'));
@@ -204,21 +204,21 @@ router.get('/groups', authenticate, async (req, res) => {
 });
 
 router.post('/report', authenticate, upload.single('file'), async (req, res) => {
-    const {subject, schoolClass} = req.query;
+    const { subject, schoolClass } = req.query;
     const userId = req.user.id
 
-    const existingGradesGroupsReports = await GradesGroupsReports.findOne({subject, schoolClass, userId});
+    const existingGradesGroupsReports = await GradesGroupsReports.findOne({ subject, schoolClass, userId });
 
     if (existingGradesGroupsReports) {
         await GradesGroupsReports.findOneAndUpdate(
-            {subject, schoolClass, userId},
-            {subject, schoolClass, userId},
-            {upsert: true, new: true}
+            { subject, schoolClass, userId },
+            { subject, schoolClass, userId },
+            { upsert: true, new: true }
         );
 
         res.status(200).json(getSuccessResponse({}, `Звіт успішно оновлений!`));
     } else {
-        const gradesGroupsReports = new GradesGroupsReports({subject, schoolClass, userId});
+        const gradesGroupsReports = new GradesGroupsReports({ subject, schoolClass, userId });
         await gradesGroupsReports.save();
 
         res.status(200).json(getSuccessResponse({}, `Звіт успішно доданий!`));
@@ -226,12 +226,12 @@ router.post('/report', authenticate, upload.single('file'), async (req, res) => 
 });
 
 router.get('/report', authenticate, async (req, res) => {
-    const {_id} = req.query
+    const { _id } = req.query
 
     const existingGradesGroupsReports = await GradesGroupsReports.findById(_id);
 
     if (existingGradesGroupsReports) {
-        const {userId, subject, schoolClass} = existingGradesGroupsReports
+        const { userId, subject, schoolClass } = existingGradesGroupsReports
 
         try {
             const filePath = path.join(__dirname, `../uploads/${userId}_${subject}_${schoolClass}.xlsx`);
@@ -247,9 +247,9 @@ router.get('/report', authenticate, async (req, res) => {
             const workbook = xlsx.readFile(filePath);
             let names = {};
             let report = []
-            const {groups} = await Groups.findOne({subject, schoolClass});
+            const { groups } = await Groups.findOne({ subject, schoolClass });
 
-            for (let listNum = 0; listNum < workbook.SheetNames?.length; listNum++){
+            for (let listNum = 0; listNum < workbook.SheetNames?.length; listNum++) {
                 // Предполагаем, что нам нужен первый лист (можно изменить по необходимости)
                 const sheetName = workbook.SheetNames[listNum];
                 let file = workbook.Sheets[sheetName];
@@ -260,7 +260,7 @@ router.get('/report', authenticate, async (req, res) => {
                     const [currentColumn, currentRow] = key.match(/[A-Za-z]+|[0-9]+/g);
                     const rowNumber = Number(currentRow);
                     if (rowNumber >= 4 && key.includes('A') && Number.isInteger(Number(key[1]))) {
-                        if (!names[key]){
+                        if (!names[key]) {
                             names[key] = {
                                 name: file[key]['v'],
                                 ratings: []  // сразу добавляем пустой массив для рейтингов
@@ -276,16 +276,16 @@ router.get('/report', authenticate, async (req, res) => {
                         const rating = Number.isInteger(Number(file[key]?.['v'])) ? Number(file[key]?.['v']) : '';
 
                         if (!ignoreTypes.includes(type) && rating) {
-                            names[rowKey].ratings.push({type: type || '', rating: rating, date: date || ''});
+                            names[rowKey].ratings.push({ type: type || '', rating: rating, date: date || '' });
                         }
                     }
                 }
             }
 
             // Преобразование дат
-            const normalReport = Object.values(names).map(({name, ratings = []}) => ({
+            const normalReport = Object.values(names).map(({ name, ratings = [] }) => ({
                 name,
-                ratings: ratings.map(({date, rating}) => {
+                ratings: ratings.map(({ date, rating }) => {
                     const [day, month, year] = date.split('/').map(Number);
                     return {
                         rating: rating,
@@ -301,7 +301,7 @@ router.get('/report', authenticate, async (req, res) => {
                     name: student.name,
                     groups: groups.map(group => {
                         // Фильтруем оценки, соответствующие датам в группе
-                        const relevantRatings = student.ratings.filter(({date}) => {
+                        const relevantRatings = student.ratings.filter(({ date }) => {
                             const dateInGroup = date => {
                                 const parsedDate = new Date(date);
                                 return group.assigment_dates.some(gDate => parsedDate.getTime() === new Date(gDate).getTime()) ||
@@ -311,15 +311,19 @@ router.get('/report', authenticate, async (req, res) => {
                         });
 
                         // Разделяем оценки по типам
-                        const assigments = relevantRatings.filter(({date}) => group.assigment_dates.some((b_date) => new Date(b_date).getTime() === new Date(date).getTime()));
-                        const controls = relevantRatings.filter(({date}) => group.control_dates.some((b_date) => new Date(b_date).getTime() === new Date(date).getTime()));
+                        const assigments = relevantRatings.filter(({ date }) => {
+                            return group.assigment_dates.some((b_date) => new Date(b_date).toISOString().split('T')[0] === new Date(date).toISOString().split('T')[0]);
+                        });
 
-                        const assigmentsAvg = assigments.reduce((sum, {rating}) => sum + (Number(rating) || 0), 0) / (assigments.length || 1);
-                        const controlsAvg = controls.reduce((sum, {rating}) => sum + (Number(rating) || 0), 0) / (controls.length || 1);
+                        const controls = relevantRatings.filter(({ date }) => {
+                            return group.control_dates.some((b_date) => new Date(b_date).toISOString().split('T')[0] === new Date(date).toISOString().split('T')[0]);
+                        });
+                        const assigmentsAvg = assigments.reduce((sum, { rating }) => sum + (Number(rating) || 0), 0) / (assigments.length || 1);
+                        const controlsAvg = controls.reduce((sum, { rating }) => sum + (Number(rating) || 0), 0) / (controls.length || 1);
 
                         let groupAvg = controls.length ? Math.round((assigmentsAvg + controlsAvg) / 2) : Math.round(assigmentsAvg);
 
-                        if (groupAvg === 0){
+                        if (groupAvg === 0) {
                             groupAvg = ''
                         } else {
                             avgAllGroups.push(groupAvg)
@@ -327,8 +331,8 @@ router.get('/report', authenticate, async (req, res) => {
 
                         return {
                             group: group.name,
-                            assigments: [...assigments, {rating: assigmentsAvg, type: 'Середня', date: ''}],
-                            controls: [...controls, {rating: controlsAvg, type: 'Середня', date: ''}],
+                            assigments: [...assigments, { rating: assigmentsAvg, type: 'Середня', date: '' }],
+                            controls: [...controls, { rating: controlsAvg, type: 'Середня', date: '' }],
                             groupAvg
                         };
                     }),
@@ -353,7 +357,7 @@ router.get('/report', authenticate, async (req, res) => {
 });
 
 router.delete('/report', authenticate, async (req, res) => {
-    const {_id} = req.query;
+    const { _id } = req.query;
 
     try {
         // Проверяем, существует ли класс
@@ -366,7 +370,7 @@ router.delete('/report', authenticate, async (req, res) => {
         // Удаляем класс
         await GradesGroupsReports.findByIdAndDelete(_id);
 
-        const {userId, subject, schoolClass} = existingGradesGroupsReports
+        const { userId, subject, schoolClass } = existingGradesGroupsReports
 
         const filePath = `uploads/${userId}_${subject}_${schoolClass}.xlsx`;
 
@@ -386,11 +390,11 @@ router.delete('/report', authenticate, async (req, res) => {
 
 router.get('/reports', authenticate, async (req, res) => {
     try {
-        const reports = await GradesGroupsReports.find({userId: req.user.id})
+        const reports = await GradesGroupsReports.find({ userId: req.user.id })
             .populate('subject', 'name') // Подгрузить только поле name из коллекции Subjects
             .populate('schoolClass', 'name'); // Подгрузить только поле name из коллекции Classes
 
-        res.status(200).json(getSuccessResponse({reports}));
+        res.status(200).json(getSuccessResponse({ reports }));
     } catch (err) {
         console.error(err);
         res.status(500).json(getErrorResponse('Сталася помилка при отриманні звіту'));
