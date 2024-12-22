7d9ab62b603705c76339a572365cfa3fd4f7f819
merlinovvv <merlinovandrej@gmail.com>
2024-12-22 14:48:16 +0200

    Admin page

--- routes/admin.js
+++ routes/admin.js
@@ -3,6 +3,7 @@ const axios = require("axios");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 const User = require("../models/User");
+const GradesGroupsReports = require("../models/GradesGroupsReports");
 const Payments = require("../models/Payments");
 const authenticate = require("../middleware/authenticate");
 const { getSuccessResponse, getErrorResponse } = require("../utils/functions");
@@ -11,26 +12,35 @@ const router = express.Router();
 
 router.get("/users", authenticate, async (req, res) => {
   const user = await User.findById(req.user.id);
-
   if (user.user_role === "admin") {
     try {
-      // Получаем всех пользователей из базы данных
       const users = await User.find().select("-password");
 
+      // Получаем всех пользователей из базы данных
+      const newUsers = await Promise.all(
+        users.map(async (us) => {
+          const reports = await GradesGroupsReports.find({ userId: us._id });
+          return {
+            ...us.toObject(),
+            reports: reports,
+          };
+        })
+      );
+
       // Возвращаем пользователей в ответе
-      res.status(200).json(getSuccessResponse({ users }));
+      res.status(200).json(getSuccessResponse({ users: newUsers }));
     } catch (err) {
       console.error("Error fetching users:", err.message);
 
       // Возвращаем ошибку при возникновении проблем
       res
         .status(500)
-        .json(getErrorResponse("Сталася помилка отримання даних користувача"));
+        .json(getErrorResponse("Сталася помилка отримання даних користувачів"));
     }
   } else {
     res
-        .status(400)
-        .json(getErrorResponse("Вам заборонено отримувати ці дані", {}));
+      .status(400)
+      .json(getErrorResponse("Вам заборонено отримувати ці дані", {}));
   }
 });
 
